a4ee669a85a866656a89d219ec33b16465ae1624
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-03-05 17:56:20 +0900

    v1.0.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import getSpacePeople from './get-space-people';
 const url = 'http://api.open-notify.org/astros.json';
- 
+exports.echo = () =>
 getSpacePeople(url)
 	.then(
 		(data) => console.log(JSON.parse(data).people),
